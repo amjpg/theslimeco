b6fdedf0451cbb13588857b06c77abf951e72d83
amjpg <mujaarjana@yahoo.com>
2025-01-18 21:02:38 -0600

    updated project

--- routes/contact.js
+++ routes/contact.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET about page. */
 router.get('/', function(req, res, next) {
-res.render('contact', { title: 'Contact The Slime Co' });
+res.render('contact', { title: 'Contact The Slime Co.' });
 });
 
 module.exports = router;
